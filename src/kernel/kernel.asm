b0b0c7863871eaed621b268b6000c97ac46d5f2d
abxxbo <devavcado@gmail.com>
2022-06-05 19:31:57 -0500

    scuffed input function

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -7,4 +7,19 @@ int 0x10
 ;; hang infinently, we don't have much to do
 jmp $
 
+;;; ---- functions ----
+
+input:
+  xor ax, ax
+  mov ah, 00h
+  int 16h
+
+  mov ch, ah
+  mov cl, al
+
+  mov ah, 0x0e
+  mov al, cl
+  int 0x10
+  jmp input
+
 times 2048-($-$$) db 0
