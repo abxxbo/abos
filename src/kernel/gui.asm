ea29c65d0a8438a4ef731480f38abd83df4cb149
abxxbo <devavcado@gmail.com>
2022-06-21 19:58:28 -0500

    Window struct

--- src/kernel/gui.asm
+++ src/kernel/gui.asm
@@ -40,9 +40,33 @@ clear:
 	ret
 
 
+
+;;; Window Structure
+;; Contains information about a singular window
+struc win
+	.x: resw 1				;; X position
+	.y: resw 1				;; Y position
+	.w: resw 1				;; Width of window,  must be less than 80
+	.h: resw 1				;; Height of window, must be less than 24
+	.focused: resb 1	;; 0x00 -> Focused
+										;; 0xFF -> Unfocused
+
+	;; Data that does not
+endstruc
+
+
+
 ;;; Clear
 Cl_nl_: db `\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n`, 0
 
 
 ;; Instructions (Data)
 instructions: db `Press Tab to switch windows | Arrow keys to move around\r\n`, 0
+
+;;; Reserve space for dummy window
+;;; TODO: actually use the struct defined
+dummy_win_x: dw 0
+dummy_win_y: dw 0
+dummy_win_w: dw 0
+dummy_win_h: dw 0
+dummy_win_focused: db 0
