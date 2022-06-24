285fa9c6645da3bd8ad222607cd3457abca9633e
abxxbo <devavcado@gmail.com>
2022-06-24 08:54:55 -0500

    Output functions

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -10,17 +10,10 @@ mov bh, 0x00
 mov bl, 0x9
 int 0x10
 
-Loop:
-	call get_char
-	call special_characters
-	mov ah, 0x0e
-	mov al, cl
-	int 0x10
-	jmp Loop
-
 jmp $
 
 %include "io/input.asm"
+%include "io/output.asm"
 
 ;; padding for size of kernel
 ;; max size allowed by bootloader is 6,144 bytes.
