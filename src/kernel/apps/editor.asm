a8713457e09f24356226af1ea9cb52bff6edd5f1
abxxbo <devavcado@gmail.com>
2022-06-26 15:54:37 -0500

    exit screen

--- src/kernel/apps/editor.asm
+++ src/kernel/apps/editor.asm
@@ -11,6 +11,9 @@ editor:
 	.MainLoop:
 		call get_char
 		mov cl, al
+		cmp cl, 0x1157	;; Shift+W
+		je exit_screen
+
 		mov ah, 0x0e
 		mov al, cl
 		int 0x10
@@ -23,6 +26,60 @@ editor:
 		jmp .MainLoop
 
 
+exit_screen:
+	;; do some stuff
+	printc `\r`
+	printc `\n`
+	;; ask user where they
+	;; would like to save file
+	mov bx, storage_q
+	call printf
+
+	mov si, 0
+	.Loop:
+		call get_char
+		mov cl, al
+		cmp cl, 13
+		je .Exit
+
+		mov ah, 0x0e
+		mov al, cl
+		int 0x10
+
+		mov byte [storage_buf+si], byte cl
+
+		jmp .Loop
+	.Exit:
+		printc `\r`
+		printc `\n`
+
+		;; clear screen / scroll down a page
+		mov bx, lotsoflines
+		call printf
+		mov bx, lotsoflines
+		call printf
+
+		mov ah, 0x02
+		mov bh, 0
+		mov dh, 0x00
+		mov dl, 0x00
+		int 0x10
+
+		;; clear command buffer
+		;; so that editor will not
+		;; be relaunched on exit
+		mov si, 0
+		jmp .KillBuffer
+		ret
+
+	.KillBuffer:
+		mov byte [buffer+si], byte 0
+		inc si
+		cmp si, 128
+		je shell
+		jne .KillBuffer
+
+	jmp $
 
 set_colors:
 	mov al, 0x03
@@ -61,3 +118,10 @@ titlebar: db 'AbEdit', 0
 spaces_titlebar: db '                                     ', 0
 
 editor_buffer: times 1024 db 0
+
+storage_buf: times 1 db 0
+storage_q: db 'Save to 3rd sector (Overwrites anything else)? ', 0
+storage_saving: db `I am saving this buffer to: `, 0
+
+lotsoflines:
+	db `\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r`, 0
