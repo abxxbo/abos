129ff1c0858fe2e33bf3bf3fec5aa17443ae46b1
abxxbo <devavcado@gmail.com>
2022-07-05 16:35:58 -0500

    remove memory det

--- src/boot/abos-boot.asm
+++ src/boot/abos-boot.asm
@@ -11,12 +11,6 @@ mov al, 0x03
 mov ah, 0
 int 0x10
 
-call detect_low
-;; Makes disk reading
-;; fail:
-; call detect_upper
-
-
 mov [BDISK], dl
 call disk_read
 jmp K_LOC     ;; assume to be 0x7e00
@@ -25,7 +19,6 @@ jmp $
 
 %include "disk.asm"
 %include "output.asm"
-%include "d_mem.asm"
 
 ;; padding
 times 510 - ($-$$) db 0
